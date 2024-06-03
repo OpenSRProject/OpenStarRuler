6be2137f45b25b5397a84e9fb7b1a4734493ba70
kb1000 <kaeptmblaubaer1000@gmail.com>
2024-06-03 23:03:22 +0200

    Include windows.h in compat/gl.h for GLU

--- source/game/compat/gl.h
+++ source/game/compat/gl.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "glad/gl.h"
+#ifdef _WIN32
+// defines APIENTRY, GL/gl.h includes it but Glad doesn't
+#  include <windows.h>
+#endif
 #if defined(__APPLE__) && defined(__MACH__)
 #  include <OpenGL/glu.h>
 #else
